19b2f502bb15886f6a9d7b89735a9961645fa29e
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2022-05-10 13:55:31 +0200

    Update workflows

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,12 @@
 const { typescript } = require('projen');
+const { NpmAccess } = require('projen/lib/javascript');
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: 'main',
   name: 'npm-module',
+  release: true,
+  publishTasks: true,
+  npmAccess: NpmAccess.PUBLIC,
+  releaseToNpm: true,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
